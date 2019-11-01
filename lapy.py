5f64d689c6ae215009e55705746d629dcb115045
BrandAllred <brandallred@gmail.com>
2019-10-31 22:53:18 -0400

    Initial commit for lapy. Stopped because I need to read over getopt to implement it correctly.

---
+++ lapy.py
@@ -0,0 +1,23 @@
+#!/env/bin/env python3
+
+
+import sys
+import getopt
+
+
+def scan_directory(directory):
+    pass
+
+
+def main(argv):
+    #optlist, args = getopt(argv, )
+    # TODO: figure out getopt, so I can do this whole thing correctly.
+    pass
+
+
+def display_help():
+    pass
+
+
+main(sys.argv)
+
